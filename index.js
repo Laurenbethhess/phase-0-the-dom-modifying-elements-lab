f5550d38469f76b9d3b25e9a11b8785a35f6d5bf
Lauren Hess <laurenbethhess@gmail.com>
2021-07-18 16:02:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1'); 
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<h1>Lauren is the champion</h1>";
+
+
+
+
+
