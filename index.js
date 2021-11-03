1a29ee77e50f9d6dba7c3e01208083d0088455ef
Lauren Hess <laurenbethhess@gmail.com>
2021-11-03 09:03:07 -0400

    commit tree 094881f209d650e9da720a2a44bc46fbae1e3fb3

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-document.querySelector('main').remove();
-
-let newHeader = document.createElement('h1'); 
-
-newHeader.id = "victory";
-
-newHeader.innerHTML = "<h1>Lauren is the champion</h1>";
-
+document.querySelector('#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'LAUREN is the champion'
 
 
 
